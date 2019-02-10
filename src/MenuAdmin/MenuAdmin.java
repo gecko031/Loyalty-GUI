936c98872818d1c77bd2e0620d72ad6028356a88
gecko031 <pawel.slepaczuk@pollub.edu.pl>
2019-02-10 21:55:58 +0100

    MenuAdmin in progress

--- src/MenuAdmin/MenuAdmin.java
+++ src/MenuAdmin/MenuAdmin.java
@@ -1,18 +1,68 @@
 package MenuAdmin;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MenuAdmin extends JFrame{
-    private JPanel contentPanel;
-    private JPanel panelRoot;
+public class MenuAdmin extends JDialog{
+
+    public JPanel panelRoot;
+    private JPanel panel1;
+    private JPanel panel2;
+    private JButton buttonOK;
+    private JButton buttonCancel;
+
+    private JPanel panelOne;
+    private JPanel panelTwo;
+
 
     public MenuAdmin() {
-        add(contentPanel);
+        //panelRoot.add(panelOne, "1");
+        //panelRoot.add(panelTwo, "2");
 
-        setTitle("Admin Account");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE );
-        setSize(300,200);
-        setLocation(825,425);
+        buttonOK.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {onOK();}
+        });
+
+        buttonCancel.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {onCancel();}
+        });
+
+
+
     }
 
+
+
+    private void onOK() {
+// add your code here
+        dispose();
+    }
+
+    private void onCancel() {
+// add your code here if necessary
+        dispose();
+    }
+
+    public static void main(String[] args) {
+        MenuAdmin dialog = new MenuAdmin();
+        dialog.pack();
+        dialog.setVisible(true);
+        System.exit(0);
+    }
+    /*public static void main(String[] args){
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new MenuAdmin();
+            }
+        });
+
+    }*/
+    private void createUIComponents() {
+        panelOne = new PanelOne().getPanelOne();
+        panelTwo = new PanelTwo().getPanelTwo();
+
+    }
 }
